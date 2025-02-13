a0219f4cc5ea2a0c61d2f936adfefd9adba32e57
Gabby Cordelli <gec2tp@virginia.edu>
2025-02-13 14:44:51 -0500

    integrated all song fiels for tokenization

---
+++ Artists/Songs/PreProcessing for ML.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import os
+import requests
+from io import StringIO
+import matplotlib
+
+filenames = ["Amanda.csv", "BabyGotBack.csv", "Ben.csv", "Bump,Bump,Bump.csv",  "DukeOfEarl.csv", "ComeOnEileen.csv","GetBusy.csv", "HelpMe,Rhonda.csv","LetMeLoveYou.csv", "MyLove.csv","OnlyGirlInTheWorld.csv", "PennyLane.csv", "ShakeItOff.csv", "SlowJamz.csv", "TheStreak.csv"]
+
+git = "https://github.com/GEMcordelli/Personal-Projects/tree/main/Artists/Songs"
+
+combined = []
+
+for file in filenames:
+    read = pd.read_csv(file)
+    combined.append(read)
+    
+    
+combined
+    
+combined = pd.concat(combined)
+
+combined = pd.DataFrame(combined)
+
+# save as csv in Songs Folder
+
+combined.to_csv("AllSongs.csv", index=False)
+
+# Clean Out Lyric Annotations
+
+cleaning = combined[~combined["Content"].str.contains("[")]
+combined.columns
