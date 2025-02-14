457dd3ab27fd1b36f8bc2d7afff66f2ea78c1c2d
Gabby Cordelli <gec2tp@virginia.edu>
2025-02-14 13:08:25 -0500

    added to Ml preprocessing; made some outputs including a token table for some basic lyrical EDA

--- Artists/Songs/PreProcessing for ML.py
+++ Artists/Songs/PreProcessing for ML.py
@@ -30,6 +30,17 @@ combined.to_csv("AllSongs.csv", index=False)
 
 cleaning = combined
 
-cleaning["chunk_str"] = cleaning.Content.str.replace(r"\['", ' ', regex=True).str.strip()
-cleaning["chunk_str"] = cleaning.Content.str.replace(r'\]', ' ', regex=True).str.strip()
-cleaning.chunk_str
+cleaning["chunk_str"] = cleaning.Content.str.replace(r"[\[\]\,\/\\\"]", ' ', regex=True)  # Remove brackets, comma, slash, backslash, quotes
+cleaning["chunk_str"] = cleaning["chunk_str"].str.replace(r"^\s+|\s+$", "", regex=True)  # Strip leading and trailing whitespaces
+cleaning = cleaning.drop(columns = ["Content"])
+
+# save as a text file then import using open and split
+cleaning.to_csv("AllSongs_Cleaned.csv", sep = "\t", index = False)
+
+# tokenize for word frequency (we dont want this to be our only table; removes important stylistic information, i.e. parentheses, ..., etc.)
+K = cleaning.chunk_str.str.split(expand=True).stack().to_frame('token_str')
+K.index.names = ['chunk_num','token_num']
+# looks like tokenizing might have actually kept in some of the key lyrical annotations! This can serve useful for other analysis
+K.iloc[800:820]
+
+K.to_csv("TOKENS_AllSongs.csv", index = False)
